a3ccf899b958087fa33095303c2024260325e538
Jorge Silva <jorge@fcn.edu.br>
2019-08-14 01:06:35 -0300

    Unreal com Python
    Utilizando script API Unreal em python, na engine unreal 4

---
+++ Content/Python/init_unreal.py
@@ -0,0 +1,9 @@
+import unreal
+
+def log_hello_unreal():
+    '''
+    Logs hello unreal para a saida de log no unreal
+    '''
+    unreal.log_warning("Hello unreal")
+
+log_hello_unreal()
---
+++ Content/Python/unreal_test.py
@@ -0,0 +1,7 @@
+import unreal
+
+def log_hello_unreal():
+    '''
+    Logs hello unreal para a saida de log no unreal
+    '''
+    unreal.log("Hello unreal")
